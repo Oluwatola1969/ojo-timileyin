bf09b984c4065f85499f8b5118bdacbcf59a7faf
Senbadejo Oluwatola <tolamide29@gmail.com>
2023-04-02 12:11:42 +0100

    active nav added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import './nav.css';
 
 
 export default function Nav() {
+const [activeClass, setActiveClass] = useState('#home')
+
   return (
     <nav id="#home">
         < div className="container__nav container">
@@ -10,16 +12,16 @@ export default function Nav() {
                 <a href="#"><h1>Ojo Timilehin</h1></a>
                 <ul className="navLinks">
                     <li>
-                        <a href="#">Welcome</a>
+                        <a href="#home" onClick={()=> setActiveClass('#home')} className={activeClass === '#home'? 'active' : ''}>Welcome</a>
                     </li>
                     <li>
-                        <a href="#about">About</a>
+                        <a href="#about" onClick={()=> setActiveClass('#about')} className={activeClass === '#about' ? 'active' : ''}>About</a>
                     </li>
                     <li>
-                        <a href="#portfolio">Portfolio</a>
+                        <a href="#portfolio" onClick={()=> setActiveClass('#portfolio')} className={activeClass === '#portfolio' ? 'active' : ''}>Portfolio</a>
                     </li>
                     <li className="contact">
-                        <a href="#contact" >Contact</a>
+                        <a href="#contact" onClick={()=> setActiveClass('#contact')} className={activeClass === '#contact' ? 'active' : ''} >Contact</a>
                     </li>
                 </ul>
             
